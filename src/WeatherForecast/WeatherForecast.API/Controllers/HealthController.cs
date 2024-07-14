2311825326d2ab3fb735d6c840a496ca6c67fa8c
Asier <asiergarciab@outlook.com>
2024-07-14 15:53:07 +0200

    feat:

---
+++ src/WeatherForecast/WeatherForecast.API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.API;
+
+[ApiController]
+public class HealthController : Controller
+{
+    [HttpGet("/health")]
+    public IActionResult Health()
+    {
+        return Ok("Healthy");
+    }
+}
